6819e91ddb89d29db3b45b84a5fa6d43f362e2d0
eve-link <eve.link.1992@gmail.com>
2014-09-16 15:59:28 +0300

    basic html

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head> 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Harjutus-4</title>
+</head>
+
+<body> 
+  <h1>Andmetüübid</h1>
+  <h2>Arvutamine murd ja- täisarvudega</h2>
+
+</body>
+</html> 
